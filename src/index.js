2aba07ab1a61aa6f90175699e5e94ba695aa70ba
Philip Shao <shaoster@gmail.com>
2021-04-06 20:29:04 -0400

    Use hash routing for gh pages.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { STATIC_ROOT } from './Constants';
+import {
+  STATIC_ROOT
+} from './Constants';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
